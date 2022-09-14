6710668d90d44e7b173f78ac9c1ba6760ef9e866
Wilson Elijah Nduka <wilsonelijah2019@gmail.com>
2022-09-14 08:59:31 -0700

    updated my code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,11 @@
 
 /**
  
-
-   * print_last_digit - prints the last digit of a number
-   * @n: n is an integer
-   * Description: prints the last digit of a number
-   * Return: integer
-   */
+ * print_last_digit - prints the last digit of a number
+ * @n: n is an integer
+ * Description: prints the last digit of a number
+ * Return: integer
+ */
 
 int print_last_digit(int n)
 {
